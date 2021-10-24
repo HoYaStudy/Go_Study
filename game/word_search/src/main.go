96279ffe7edebe9729593323f9617a289b496be9
llHoYall <hoya128@gmail.com>
2021-10-24 16:48:39 +0900

    Added Project: Game/Word Search
    Searching a word

---
+++ game/word_search/src/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Error: Wrong Arguments!")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("----------------------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("  ", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("----------------------------------------")
+		fmt.Println()
+	}
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+	filelist, err := filepath.Glob(path)
+	if err != nil {
+		fmt.Println("Error: File Not Found! - ", err)
+		return findInfos
+	}
+
+	ch := make(chan FindInfo)
+	cnt := len(filelist)
+	recvCnt := 0
+
+	for _, filename := range filelist {
+		go FindWordInFile(word, filename, ch)
+	}
+
+	for findInfo := range ch {
+		findInfos = append(findInfos, findInfo)
+		recvCnt++
+		if recvCnt == cnt {
+			break
+		}
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string, ch chan FindInfo) {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error: File Not Found! - ", filename)
+		ch <- findInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	ch <- findInfo
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
